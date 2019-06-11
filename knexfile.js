a272d8f0f60a3af234b2342410e533b7815a72b7
Slim Hmidi <slim.hmidi1@gmail.com>
2019-06-12 00:14:33 +0200

    fix lint errors

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,22 @@
 module.exports = {
-    development: {
-      client: 'pg',
-      useNullAsDefault: true,
-      connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'admin',
-        database: 'joindb',
-      },
+  development: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: 'admin',
+      database: 'joindb',
     },
-    test: {
-      client: 'pg',
-      useNullAsDefault: true,
-      connection: {
-        host: '127.0.0.1',
-        user: 'postgres',
-        password: 'admin',
-        database: 'jointestdb',
-      },
+  },
+  test: {
+    client: 'pg',
+    useNullAsDefault: true,
+    connection: {
+      host: '127.0.0.1',
+      user: 'postgres',
+      password: 'admin',
+      database: 'jointestdb',
     },
-  };
-  
+  },
+};
